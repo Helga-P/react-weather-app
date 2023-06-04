1334480c0286573761e1de7dc7f80eb8171b5ae2
Helga-P <124738568+Helga-P@users.noreply.github.com>
2023-06-04 18:33:22 +0100

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import ThemeProvider from "react-bootstrap/ThemeProvider";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
